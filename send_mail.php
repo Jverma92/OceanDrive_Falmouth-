a987ea093edf1023c7610cb3e92f6b7775d49ba6
Jonathan Verma <jonjverma92@gmail.com>
2018-07-30 13:36:56 +0100

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,20 +1,17 @@
 <?php
 /*
-This first bit sets the email address that you want the form to be submitted to.
-You will need to change this value to a valid email address that you can access.
+sets the email address that you want the form to be submitted to.
 */
 $webmaster_email = "oceandrivefalmouth@gmail.com";
 
 /*
-This bit sets the URLs of the supporting pages.
-If you change the names of any of the pages, you will need to change the values here.
+sets the URLs of the supporting pages.
 */
 $feedback_page = "index.php";
 $error_page = "error_message.html";
 $thankyou_page = "thank_you.html";
 
-/*
-This next bit loads the form field data into variables.
+/*.
 If you add a form field, you will need to add it here.
 */
 $email_address = $_REQUEST['email_address'] ;
